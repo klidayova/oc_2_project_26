87caf793252170e8bed62491cd79907e49523a5b
Kristina Lidayová <kristina.lidayova@scilifelab.se>
2025-03-07 16:29:19 +0100

    Cleaned and commentedversion of detecting FP in EM

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -4,7 +4,15 @@ import open3d as o3d
 import matplotlib.pyplot as plt
 import xml.etree.ElementTree as ET
 
-def plot_image(image, size):    
+color_values = [ '-154', '-3611080', '-13210', '-16777088', '-16744193', '-11776948', '-65408',
+  '-8388480', '-10040065',    '-39322',  '-1644826', '-13421773', '-16760704',  '-8372224',
+ '-15132391',  '-3342490',    '-65536', '-10066177',  '-65281', '-16711936', '-10066330',
+ '-16744320', '-16711681', '-12550144',  '-8421505',  '-6710887',  '-8323328',    '-32768',
+  '-8388353', '-16776961',  '-3381505', '-10027060',      '-256', '-10027009', '-16744448',
+ '-16744384', '-10027162', '-16777216',  '-8388544', '-12582784',    '-36913', '-16711808',
+  '-3355444',        '-1']
+
+def plot_image(image, size=40):    
     """
     Plot a grayscale image in the original size - takes time
     """
@@ -20,15 +28,16 @@ def xml_to_dataframe(xml_file):
 
     # Initialize lists to store data
     data = []
-    columns = ['id', 'name', 'pos_x', 'pos_y']
+    columns = ['id', 'name', 'pos_x', 'pos_y', 'color']
 
     # Iterate through each 'roi' element
     for roi in root.findall('roi'):
         roi_data = {
             'id': roi.find('id').text,
             'name': roi.find('name').text,
-            'pos_x': roi.find('position/pos_y').text,
-            'pos_y': roi.find('position/pos_x').text
+            'pos_x': roi.find('position/pos_x').text,
+            'pos_y': roi.find('position/pos_y').text,
+            'color': roi.find('color').text
         }
         data.append(roi_data)
 
@@ -39,6 +48,7 @@ def xml_to_dataframe(xml_file):
     df['id'] = pd.to_numeric(df['id'])
     df['pos_x'] = pd.to_numeric(df['pos_x'])
     df['pos_y'] = pd.to_numeric(df['pos_y'])
+    df['color'] = pd.to_numeric(df['color'])
 
     return df
 
@@ -51,11 +61,41 @@ def dataframe_to_xml(df, filename=None):
         
         ET.SubElement(roi, 'classname').text = 'plugins.kernel.roi.roi2d.ROI2DPoint'
         ET.SubElement(roi, 'id').text = str(index)  # Increment id for each point
-        ET.SubElement(roi, 'name').text = 'Point2D'
+        ET.SubElement(roi, 'name').text = 'Point'+str(index+1)
         ET.SubElement(roi, 'selected').text = 'true'
         ET.SubElement(roi, 'readOnly').text = 'false'
         ET.SubElement(roi, 'properties')
-        ET.SubElement(roi, 'color').text = '-20388296' #'-16711936' green, '-65536' red,  '-16776961' blue
+        ET.SubElement(roi, 'color').text = color_values[index % len(color_values)]
+        ET.SubElement(roi, 'stroke').text = '2'
+        ET.SubElement(roi, 'opacity').text = '0.3'
+        ET.SubElement(roi, 'showName').text = 'false'
+        ET.SubElement(roi, 'z').text = '-1'
+        ET.SubElement(roi, 't').text = '-1'
+        ET.SubElement(roi, 'c').text = '-1'
+        
+        position = ET.SubElement(roi, 'position')
+        ET.SubElement(position, 'pos_x').text = str(row['pos_y'])
+        ET.SubElement(position, 'pos_y').text = str(row['pos_x'])
+
+    # Create the XML tree and save to file
+    tree = ET.ElementTree(root)
+    if filename is not None:
+        tree.write(filename, encoding='utf-8', xml_declaration=True)
+
+def dataframe_to_xml_(df, filename=None):
+    # Create the root element
+    root = ET.Element('root')
+
+    for index, row in df.iterrows():
+        roi = ET.SubElement(root, 'roi')
+        
+        ET.SubElement(roi, 'classname').text = 'plugins.kernel.roi.roi2d.ROI2DPoint'
+        ET.SubElement(roi, 'id').text = str(index)  # Increment id for each point
+        ET.SubElement(roi, 'name').text = 'Point'+str(index+1)
+        ET.SubElement(roi, 'selected').text = 'true'
+        ET.SubElement(roi, 'readOnly').text = 'false'
+        ET.SubElement(roi, 'properties')
+        ET.SubElement(roi, 'color').text = color_values[index % len(color_values)]# '-20388296' #'-16711936' green, '-65536' red,  '-16776961' blue
         ET.SubElement(roi, 'stroke').text = '2'
         ET.SubElement(roi, 'opacity').text = '0.3'
         ET.SubElement(roi, 'showName').text = 'false'
